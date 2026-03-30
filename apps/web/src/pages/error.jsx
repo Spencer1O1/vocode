c738cf1721ccb0acfb446d65d271e57775db7977
Spencer Smith <spencerliamsmith@gmail.com>
2026-03-30 03:19:02 -0600

    fix linter errors in site

--- apps/web/src/pages/error.jsx
+++ apps/web/src/pages/error.jsx
@@ -1,15 +1,21 @@
-
-
 function Error() {
-    return <>
-        <main>
-            <h1 className="text-6xl text-white bg-black py-3">This page is not valid.</h1>
-            <br/>
-            <img className="mx-auto" src="https://media.tenor.com/-iiMZcIHkE8AAAAM/sad-emoji.gif" alt="sadge emoji"/>
-            <br/>
-            <a href="/" className="text-sky-400 underline">Back to home</a>
-        </main>
-    </>
+  return (
+    <main>
+      <h1 className="text-6xl text-white bg-black py-3">
+        This page is not valid.
+      </h1>
+      <br />
+      <img
+        className="mx-auto"
+        src="https://media.tenor.com/-iiMZcIHkE8AAAAM/sad-emoji.gif"
+        alt="sadge emoji"
+      />
+      <br />
+      <a href="/" className="text-sky-400 underline">
+        Back to home
+      </a>
+    </main>
+  );
 }
 
-export default Error
+export default Error;
